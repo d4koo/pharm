2c6ca4a15a5332cc12bf6bafa5005edb6dba6112
1vzl1 <victor.zhenyu.li@gmail.com>
2017-02-18 14:00:22 -0500

    a

--- messages/index.js
+++ messages/index.js
@@ -6,7 +6,7 @@ http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 -----------------------------------------------------------------------------*/
 "use strict";
 const Dialog = require('./dialog.js');
-const Symtpoms = require('./symtpoms.js');
+const Symp = require('./symptoms.js');
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 
