ce2ea7306edd26e8e1e82714e2c2c6b0402e9722
Donald <donaldkoo72@gmail.com>
2017-02-18 18:09:39 -0500

    Update dialog.js

--- messages/dialog.js
+++ messages/dialog.js
@@ -4,7 +4,23 @@ module.exports={
   askSymptoms: "What types of symptoms are you experiencing?",
   guessDiagnosis: "Based on your symptoms, I think you may experiencing the following: ",
   bestMeds: "Here's the best medication for this diagnosis: ",
-  medsList: ["Tylenol"," Advil"," See a doctor"],
+  
+  medsDepression: "",
+  medsHeart: "" ,
+  medsCold: "",
+  medsHead: "",
+  medsFood: "",
+  medsReflux: "",
+  medsInflam: "",
+  medsKidStn: "",
+  medsFlu: "",
+  medsMono: "",
+  medsDisc: "",
+  medsFear: "",
+  medsBloat: "",
+  medsAche: "",
+  medsCancer:"",
+  
   findPharms: "Here are the closest pharmacies for you to pick up medicine: ",
   endMessage: "Feel better!"
 }
