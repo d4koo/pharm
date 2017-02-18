0b504ba36e88e1a4f8fda9beea22f9e3947d2e75
1vzl1 <victor.zhenyu.li@gmail.com>
2017-02-18 13:08:37 -0500

    symtpoms adding

--- messages/index.js
+++ messages/index.js
@@ -6,6 +6,7 @@ http://docs.botframework.com/builder/node/guides/understanding-natural-language/
 -----------------------------------------------------------------------------*/
 "use strict";
 const Dialog = require('./dialog.js');
+var Symtpoms = require('./symtpoms.js');
 var builder = require("botbuilder");
 var botbuilder_azure = require("botbuilder-azure");
 
